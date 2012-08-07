6ab77b66de3277556cf2c007692b44b45d4c5fbd
Paul Liu <rocliulp@gmail.com>
2012-08-07 21:33:42 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+int gcd (int u, int v) {
+  int t = 0;
+  while (u > 0) {
+    if (u < v) {
+      t = u;
+      u = v;
+      v = t;
+    }
+    u = u - v;
+  }
+  return v;
+}
+
+int main (int argc, char * argv []) {
+  int x = 0;
+  int y = 0;
+  while (scanf ("%d %d", &x, &y) != EOF) {
+    if (x > 0 && y > 0) {
+      printf ("%d %d %d\n\r", x, y, gcd (x, y));
+    }
+  }
+  return 0;
+}
